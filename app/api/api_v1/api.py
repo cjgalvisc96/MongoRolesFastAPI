44ce2d8bbcf725317c76f27bfe8eb8eb55bf2646
Cristian Galvis <cgalvis@playvox.com>
2021-10-27 08:47:08 -0500

    Add test_roles::test_get_all_roles_by_authorised_user

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.routers import accounts, auth
+from app.api.api_v1.routers import accounts, auth, roles
 
 api_router = APIRouter()
 api_router.include_router(auth.router)
 api_router.include_router(accounts.router)
+api_router.include_router(roles.router)
---
+++ app/api/api_v1/routers/roles.py
@@ -0,0 +1,26 @@
+
+  
+from typing import Any, List
+
+from fastapi import APIRouter, Security
+from app import crud, schemas, models
+from app.api import deps
+from app.constants.role import Role
+
+router = APIRouter(prefix="/roles", tags=["roles"])
+
+
+@router.get("", response_model=List[schemas.Role])
+async def get_roles(
+    skip: int = 0,
+    limit: int = 100,
+    current_user: models.User = Security(
+        deps.get_current_active_user,
+        scopes=[Role.ADMIN["name"], Role.SUPER_ADMIN["name"]],
+    ),
+) -> Any:
+    """
+    Retrieve all available user roles.
+    """
+    roles = await crud.role.get_multi(skip=skip, limit=limit)
+    return roles
